01b436549f8721d505afae43f740eba08b5dbf4a
7oseiinyappout <a.elhusseiny@appout.co>
2025-04-01 19:00:20 +0200

    s3

--- src/configs/aws.js
+++ src/configs/aws.js
@@ -7,7 +7,7 @@ const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SERET_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY,
   },
 });
 
