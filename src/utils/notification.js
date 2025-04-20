04d6e5cda99ad5d4f58f75391e398bf728cba655
ahmed elhoseiny <a.elhusseiny@appout.co>
2025-04-20 12:43:18 +0200

    url n

--- src/utils/notification.js
+++ src/utils/notification.js
@@ -3,7 +3,7 @@ const axios = require("axios")
 exports.send = (body,req) =>{
     const protocol = req.protocol; // http or https
     const host = req.get('host'); 
-    axios.post(`${protocol}://${host}/sendnotification`,body).catch((e)=>{
+    axios.post(`https://loginsystem-production-9118.up.railway.app/sendnotification`,body).catch((e)=>{
         console.log("error in sending notification" + e)
     })
 }
